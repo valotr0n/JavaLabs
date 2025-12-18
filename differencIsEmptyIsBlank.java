d1e26b5b33fa85165b91f90f459d450a64064b1c
Valera <154832522+valotr0n@users.noreply.github.com>
2025-12-18 23:54:26 +0300

    Delete differencIsEmptyIsBlank.java

--- differencIsEmptyIsBlank.java
+++
@@ -1,8 +0,0 @@
-public class differencIsEmptyIsBlank {
-    public static void main(String[] args) {
-        String stringIsEmpty = "";
-        String stringIsBlank = "  ";
-        System.out.println(stringIsEmpty.isEmpty());
-        System.out.println(stringIsBlank.isBlank());
-    }
-}
